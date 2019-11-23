a8c4db3e066e6ec20dc330772a8e720416d34628
Enoch Nyako <nyakoapps@gmail.com>
2019-11-23 18:56:14 +0000

    update header layout to be more responsive

--- components/Header.js
+++ components/Header.js
@@ -8,8 +8,8 @@ function Header() {
                     PM2R
             </span>
         </button>
-        <div className="flex flex-grow items-center justify-end p-3 pl-6 pr-6 ">
-            <div className="self-start mr-auto h-full">
+        <div className="flex flex-grow items-center justify-between p-3 pl-6 pr-6 ">
+            <div className="self-start h-full">
                 <button className="border-gray-400 border-2 rounded h-full pl-2 pr-2 ">
                     Top Shoes Ltd
                 </button>
@@ -19,14 +19,15 @@ function Header() {
                 </ul>
             </div>
 
-
-            <button className="rounded h-full pl-2 pr-2 flex bg-red-600 text-white  ">
-                Red button
-            </button>
-            <button className="h-full rounded  pl-2 pr-2 flex items-center justify-end">
-                <img alt="User icon" className="h-full w-1/5 rounded-full" src="/static/boy.svg"/>
-                <span className="ml-2 text-xl">James Hardock</span>
-            </button>
+            <div className="flex items-center justify-end h-full">
+                <button className="rounded h-full pl-2 pr-2 flex items-center bg-red-600 text-white  ">
+                    Red button
+                </button>
+                <button className="h-full rounded  pl-2 pr-2 flex items-center justify-end">
+                    <img alt="User icon" className="h-full w-1/5 rounded-full" src="/static/boy.svg"/>
+                    <span className="ml-2 text-xl">James Hardock</span>
+                </button>
+            </div>
 
 
         </div>
