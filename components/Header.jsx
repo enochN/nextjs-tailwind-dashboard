8f7bfc46e666f63bc89f91b66cb7a1f4284df30f
Enoch Nyako <nyakoapps@gmail.com>
2019-11-24 16:15:05 +0000

    update focus and hover styles for Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
         </button>
         <div className="flex flex-grow items-center justify-between p-3 pl-6 pr-6 ">
             <div className="self-start h-full">
-                <button className="border-gray-400 border-2 rounded h-full pl-2 pr-2 ">
+                <button className="border-gray-400 border-2 rounded h-full px-2 focus:bg-gray-100 hover:bg-gray-100">
                     Top Shoes Ltd
                 </button>
                 <ul className="hidden">
@@ -20,8 +20,9 @@ function Header() {
             </div>
 
             <div className="flex items-center justify-end h-full">
-                <button className="rounded h-full pl-2 pr-2 flex items-center bg-red-600 text-white  ">
-                    Red button
+                <button
+                    className="rounded h-full px-4 flex items-center bg-red-600 text-white focus:bg-red-700 hover:bg-red-700 ">
+                    Test Mode
                 </button>
                 <button className="h-full rounded  pl-2 pr-2 flex items-center justify-end">
                     <img alt="User icon" className="h-full w-1/5 rounded-full" src="/static/boy.svg"/>
